1d5ebebdc114d9c7749be23d3c63a3f593e153d0
roman <ramoni1990@gmail.com>
2020-01-13 22:36:38 +0300

    generate project

---
+++ src/main/java/me/kolganov/grannyshome/GrannysHomeApplication.java
@@ -0,0 +1,13 @@
+package me.kolganov.grannyshome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrannysHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrannysHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/kolganov/grannyshome/GrannysHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kolganov.grannyshome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrannysHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
