a6fa22140409192ad1140b7ed55fc3d3e6b117bf
roman <ramoni1990@gmail.com>
2020-01-30 23:13:13 +0300

    Личная переписка - Багу немного пофиксил

--- src/main/java/me/kolganov/grannyshome/rest/ChatController.java
+++ src/main/java/me/kolganov/grannyshome/rest/ChatController.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import me.kolganov.grannyshome.rest.dto.MessageDto;
 import me.kolganov.grannyshome.rest.dto.UserDto;
 import me.kolganov.grannyshome.service.MessageService;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.security.Principal;
 
@@ -17,17 +19,11 @@ import java.security.Principal;
 public class ChatController {
     private final MessageService messageService;
 
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public MessageDto sendMessage(@Payload MessageDto messageDto) {
+    @MessageMapping("/chat.sendMessage/{dialog_id}")
+    @SendTo("/topic/public/{dialog_id}")
+    public MessageDto sendMessage(@Payload MessageDto messageDto, @DestinationVariable("dialog_id") String dialogId) {
+        System.out.println(dialogId);
         messageService.save(MessageDto.toEntity(messageDto));
-        return messageDto;
-    }
-
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public MessageDto addUser(@Payload MessageDto messageDto, SimpMessageHeaderAccessor headerAccessor) {
-        headerAccessor.getSessionAttributes().put("username", messageDto.getUserDtoFrom().getName());
         return messageDto;
     }
 }
